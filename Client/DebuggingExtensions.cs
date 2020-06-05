847427f99bd1f07a58b0259588993c3ecdf0b612
Wojtek Henszke <wojciech.henszke@nokia.com>
2020-06-05 17:30:55 +0200

    Added Customer form and Debugging extension

---
+++ Client/DebuggingExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json;
+
+namespace PizzaSinglePageApp.Client
+{
+    public static class DebuggingExtensions
+    {
+        public static string ToJson(this object obj) => JsonSerializer.Serialize(obj, obj.GetType());
+    }
+}
